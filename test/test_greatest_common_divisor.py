e14402d3023137c330e1b50bb92e10eedd707d86
beepscore <support@beepscore.com>
2015-05-13 14:07:50 -0700

    In test_greatest_common_divisor.py add tuple test_data

--- test/test_greatest_common_divisor.py
+++ test/test_greatest_common_divisor.py
@@ -7,7 +7,11 @@ import greatest_common_divisor as gcd
 class TestGreatestCommonDivisor(unittest.TestCase):
 
     def setUp(self):
-        pass
+        # use tuple of tuples instead of list of tuples because data won't change
+        # a, b, expected
+        self.test_data = ( (12, 8, 4),
+                          #(12, 9, 3)
+                          )
 
     def test_greatest_common_divisor_zero(self):
         actual = gcd.GreatestCommonDivisor.greatest_common_divisor(12, 0)
@@ -18,8 +22,9 @@ class TestGreatestCommonDivisor(unittest.TestCase):
         self.assertEqual(0, actual)
 
     def test_greatest_common_divisor(self):
-        actual = gcd.GreatestCommonDivisor.greatest_common_divisor(12, 8)
-        self.assertEqual(4, actual)
+        for test_case in self.test_data:
+            actual = gcd.GreatestCommonDivisor.greatest_common_divisor(test_case[0], test_case[1])
+            self.assertEqual(test_case[2], actual)
 
 if __name__ == "__main__":
     unittest.main()

0d03206a8d1e1686bb6eb0eb98a7d02658314ad5
beepscore <support@beepscore.com>
2015-05-13 16:27:22 -0700

    In greatest_common_divisor.py implement greatest_common_divisor and add next_smaller_divisor. In test_greatest_common_divisor add test cases. Add test_next_smaller_divisor.

--- greatest_common_divisor.py
+++ greatest_common_divisor.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 class GreatestCommonDivisor:
     """
     Methods to return greatest common divisor of two numbers.
@@ -11,6 +12,23 @@ class GreatestCommonDivisor:
         """
         if a <= 0 or b <= 0:
             return 0
-        # simplest implementation that can pass tests.
-        # TODO Implement more general solution.
-        return 4
+
+        if b > a:
+            # swap
+            a, b = b, a
+
+        divisor = b
+        while divisor > 0:
+            if a % divisor == 0 and b % divisor == 0:
+                return divisor
+            else:
+                divisor = GreatestCommonDivisor.next_smaller_divisor(a, divisor)
+
+    def next_smaller_divisor(number, divisor):
+        index = (number // divisor) + 1
+        while index <= number:
+            if number % index == 0:
+                return number // index
+            else:
+                index += 1
+        return 1
--- test/test_greatest_common_divisor.py
+++ test/test_greatest_common_divisor.py
@@ -9,8 +9,9 @@ class TestGreatestCommonDivisor(unittest.TestCase):
     def setUp(self):
         # use tuple of tuples instead of list of tuples because data won't change
         # a, b, expected
-        self.test_data = ( (12, 8, 4),
-                          #(12, 9, 3)
+        self.test_data = ((12, 8, 4),
+                          (12, 9, 3),
+                          (54, 24, 6)
                           )
 
     def test_greatest_common_divisor_zero(self):
@@ -25,6 +26,25 @@ class TestGreatestCommonDivisor(unittest.TestCase):
         for test_case in self.test_data:
             actual = gcd.GreatestCommonDivisor.greatest_common_divisor(test_case[0], test_case[1])
             self.assertEqual(test_case[2], actual)
+
+    def test_next_smaller_divisor(self):
+        actual = gcd.GreatestCommonDivisor.next_smaller_divisor(8, 8)
+        self.assertEqual(4, actual)
+        actual = gcd.GreatestCommonDivisor.next_smaller_divisor(12, 12)
+        self.assertEqual(6, actual)
+        actual = gcd.GreatestCommonDivisor.next_smaller_divisor(12, 6)
+        self.assertEqual(4, actual)
+        actual = gcd.GreatestCommonDivisor.next_smaller_divisor(12, 4)
+        self.assertEqual(3, actual)
+        actual = gcd.GreatestCommonDivisor.next_smaller_divisor(12, 3)
+        self.assertEqual(2, actual)
+        actual = gcd.GreatestCommonDivisor.next_smaller_divisor(12, 2)
+        self.assertEqual(1, actual)
+        actual = gcd.GreatestCommonDivisor.next_smaller_divisor(12, 1)
+        self.assertEqual(1, actual)
+        actual = gcd.GreatestCommonDivisor.next_smaller_divisor(54, 18)
+        self.assertEqual(9, actual)
+
 
 if __name__ == "__main__":
     unittest.main()
